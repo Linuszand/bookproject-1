d0770e153bac448a96046f02e785e30b5df37f59
Linus <linus.zand@gmail.com>
2023-04-18 15:05:41 +0200

    third commit

--- api.js
+++ api.js
@@ -1,1 +1,16 @@
-asfasfasf
+import express from 'express'
+const api = express()
+api.use(express.json())
+
+
+import mongoose from 'mongoose'
+const connection = "mongodb+srv://linus:eQqNJRreQJXZVAt2@bookhome.yvh0dhe.mongodb.net/test"
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+api.listen(3457, () => {
+  console.log('Connected to http://localhost:3457')
+  mongoose.connect(connection, { dbName: 'clusterproject' })
+})
